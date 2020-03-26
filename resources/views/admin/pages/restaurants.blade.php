d6fc1bfcfaa942a369fe2cc539d1b6eeb1052fec
nivetha-t2s <nivetha.vadivel@touch2success.com>
2020-03-26 09:59:51 +0530

    Stores list UI design

--- resources/views/admin/pages/restaurants.blade.php
+++ resources/views/admin/pages/restaurants.blade.php
@@ -2,32 +2,32 @@
 
 @section("content")
 <div id="main">
-	<div class="page-header">
+    <div class="page-header">
+        <div class="pull-right">
+            <a href="{{URL::to('admin/restaurants/addrestaurant')}}" class="btn btn-primary">Add Store <i class="fa fa-plus"></i></a>
+        </div>
+        <h2>Stores</h2>
 
-		<div class="pull-right">
-			<a href="{{URL::to('admin/restaurants/addrestaurant')}}" class="btn btn-primary">Add Store <i class="fa fa-plus"></i></a>
-		</div>
-		<h2>Stores</h2>
-	</div>
-	@if(Session::has('flash_message'))
-				    <div class="alert alert-success">
-				    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    </div>
+    @if(Session::has('flash_message'))
+                    <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 <span aria-hidden="true">&times;</span></button>
-				        {{ Session::get('flash_message') }}
-				    </div>
-	@endif
+                        {{ Session::get('flash_message') }}
+                    </div>
+    @endif
 
 <div class="panel panel-default panel-shadow">
     <div class="panel-body">
 
-        <table id="data-table" class="table table-striped table-hover dt-responsive" cellspacing="0" width="100%">
+        <table id="order_data_table" class="table table-striped table-hover dt-responsive" cellspacing="0" width="100%">
             <thead>
             <tr>
-                <th>Name</th>
-                <th>Menu Categories</th>
-                <th>Menu Items</th>
-                <th>Orders</th>
-                <th class="text-center width-100">Action</th>
+                <th width="35%">Name</th>
+                <th width="15%">Menu Categories</th>
+                <th width="15%">Menu Items</th>
+                <th width="15%">Orders</th>
+                <th width="10%" class="text-center width-100">Action</th>
             </tr>
             </thead>
 
@@ -35,21 +35,32 @@
             @foreach($restaurants as $i => $restaurant)
             <tr>
                 <td><a href="{{ url('admin/restaurants/view/'.$restaurant->id) }}" class="text-regular">{{ $restaurant->restaurant_name }}</a>
-                    <a href="{{ url('admin/restaurants/view/'.$restaurant->id) }}" class="text-muted"><i class="md md-local-restaurant"></i></a></td>
+                    <a href="{{ url('admin/restaurants/view/'.$restaurant->id) }}" class="text-muted"><i class="md md-local-restaurant"></i></a>
+                </td>
                 <td>{{App\Restaurants::getMenuCategories($restaurant->id)}}</td>
                 <td>{{App\Restaurants::getMenuItems($restaurant->id)}}</td>
                 <td>{{App\Restaurants::getOrders($restaurant->id)}}</td>
                 <td class="text-center">
-                <a href="{{ url('admin/restaurants/view/'.$restaurant->id) }}" class="btn btn-default btn-rounded"><i class="md md-settings-display"></i></a>
-                <a href="{{ url('admin/restaurants/addrestaurant/'.$restaurant->id) }}" class="btn btn-default btn-rounded"><i class="md md-edit"></i></a>
-                <a href="{{ url('admin/restaurants/delete/'.$restaurant->id) }}" class="btn btn-default btn-rounded" onclick="return confirm('Are you sure? You will not be able to recover this.')"><i class="md md-delete"></i></a>
-            </td>
+                  <a href="{{ url('admin/restaurants/view/'.$restaurant->id) }}" class="btn btn-default btn-rounded"><i class="md md-settings-display"></i></a>
+                  <a href="{{ url('admin/restaurants/addrestaurant/'.$restaurant->id) }}" class="btn btn-default btn-rounded"><i class="md md-edit"></i></a>
+                  <a href="{{ url('admin/restaurants/delete/'.$restaurant->id) }}" class="btn btn-default btn-rounded" onclick="return confirm('Are you sure? You will not be able to recover this.')"><i class="md md-delete"></i></a>
+                </td>
 
             </tr>
            @endforeach
 
             </tbody>
-        </table>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+
+                $('#order_data_table').dataTable({
+                    "order": [[ 0, "desc" ]],
+                     "scrollX": true
+                });
+
+            } );
+         </script>
 
     </div>
     <div class="clearfix"></div>
